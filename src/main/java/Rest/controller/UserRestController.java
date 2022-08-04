10eac7bb778310a45d7d2e7638ec99b16a00d181
dshkalikov <bern1991@gmail.com>
2022-08-04 17:42:04 +0300

    Work_updateTable_04082022_2

--- src/main/java/Rest/controller/UserRestController.java
+++ src/main/java/Rest/controller/UserRestController.java
@@ -32,6 +32,7 @@ public class UserRestController {
     }
 
     @GetMapping("/userAuth")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<CustomUserDetails> getUserAuthority() {
         CustomUserDetails authorizedUser = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return new ResponseEntity<>(authorizedUser, HttpStatus.OK);
